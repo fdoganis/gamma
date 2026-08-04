e1f12e119d5c403837aaf03ad8a44dc0b2ea6c00
fdoganis <fivos.doganis@gmail.com>
2026-08-04 22:41:39 +0300

    refactor: move Game to core folder

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@
 // Import only what you need, to help your bundler optimize final code size using tree shaking
 // see https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking)
 
-import { Game } from './Game'
+import { Game } from './core/Game'
 const game = new Game();
 game.start();
